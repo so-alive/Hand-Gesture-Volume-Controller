8179fd36677b34f8fb7b90d5d25d7a650ada6398
Shailesh Dadhich <kesh4v50@gmail.com>
2026-09-12 23:08:00 +0530

    Understanding Open cv, pycaw, cvzone

---
+++ .history/index_20260912214438.py
@@ -0,0 +1,9 @@
+import cv2
+import math
+import numpy as np
+from cvzone.HandTrackingModule import HandDetector
+from pycaw.pycaw import AudioUtilities
+
+# 1. System Volume Setup
+device = AudioUtilities.GetSpeakers()
+volume = device.End
