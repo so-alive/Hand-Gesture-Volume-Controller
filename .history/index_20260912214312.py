8de13e9ddd7e5eed5fc910550ef225f4bf84a3f8
Shailesh Dadhich <kesh4v50@gmail.com>
2026-09-12 23:20:35 +0530

    Ignore VS Code local history

--- .history/index_20260912214312.py
+++
@@ -1,5 +0,0 @@
-import cv2
-import math
-import numpy as np
-from cvzone.HandTrackingModule import HandDetector
-from pycaw
--- .history/index_20260912214414.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import math
-import numpy as np
-from cvzone.HandTrackingModule import HandDetector
-from pycaw.pycaw import AudioUtilities
-
-# 1. System Volume Setup
-device = AudioUtilities
